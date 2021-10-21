7c0121b3ff90edac812d7bd02f87aa9309642151
Max <lord180499@gmail.com>
2021-10-21 13:25:19 +0300

    end

--- main.js
+++ main.js
@@ -9,7 +9,9 @@ loginDiv = document.querySelector('.login'),
 passwordInput = document.querySelector('.input-password'), 
 passwordDiv = document.querySelector('.password'),
 terminal = document.querySelector('.terminal'),
-terminalText = document.querySelector('.terminal-text');
+terminalText = document.querySelector('.terminal-text'),
+submitBtn = document.querySelector('.btn'),
+succesText = document.querySelector('.success-hide');
 
 const emailValidation = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 nameValidation = /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/, 
@@ -21,6 +23,8 @@ errorEmail = "Please enter a valid email",
 errorLogin = "Please enter a login consisting only of letters and numbers",
 errorPassword = "Please enter a password consisting of upper and lower case letters and numbers";
 
+let first  = false, email = false, pass = false, login = false;
+
 let toggleActive = (elemSelect, elemToggle) => {
     elemSelect.onfocus = () => elemToggle.classList.toggle('active');
     elemSelect.onblur = () => elemToggle.classList.toggle('active');
@@ -31,30 +35,46 @@ let getValidate = (elemInput, valueValidation) => {
         if(!valueValidation.test(elemInput.value)) {
             elemInput.style.borderBottom = '0.7vh solid red';
             terminal.style.display = 'block';
-            switch (valueValidation) {
-                case emailValidation:
-                    terminalText.textContent = errorEmail;
-                    break;
-                case nameValidation:
-                    terminalText.textContent = errorName;
-                    break;
-                case loginValidation:
-                    terminalText.textContent = errorLogin;
-                    break;
-                case passwordValidation:
-                    terminalText.textContent = errorPassword;
-                    break;
-                default:
-                    break;
-            }
+            switcher(valueValidation, true, false);
         } else {
             terminal.style.display = 'none';
             elemInput.style.borderBottom = '0.7vh solid rgb(75, 250, 52)';
+            switcher(valueValidation, false, true);
         }
+        email && first && login && pass ? submitBtn.disabled = false : submitBtn.disabled = true;
     })
 }
 
+const switcher = (valueValidation, stateErrors, stateSubmit = false) => {
+    switch (valueValidation) {
+        case emailValidation:
+            stateErrors ? terminalText.textContent = errorEmail : undefined;
+            email = stateSubmit;
+            break;
+        case nameValidation:
+            stateErrors ? terminalText.textContent = errorName : undefined;
+            first = stateSubmit;
+            break;
+        case loginValidation:
+            stateErrors ? terminalText.textContent = errorLogin : undefined;
+            login = stateSubmit;
+            break;
+        case passwordValidation:
+            stateErrors ? terminalText.textContent = errorPassword : undefined;
+            pass = stateSubmit;
+            break;
+        default:
+            break;
+    }
+}
 
+const formAccept = (button) => {
+    button.addEventListener('click', () => {
+        succesText.classList.toggle('success-show')
+    })
+}
+
+formAccept(submitBtn);
 
 toggleActive(nameInput, nameDiv);
 toggleActive(emailInput, emailDiv);
